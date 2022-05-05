4f583f154a68238d1c9c4c662549013bfab44d72
Nghia Nguyen <aihgn224@gmail.com>
2022-05-04 19:25:51 -0700

    dark/light mode successful

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleDark, toggleLight } from './features/modeSlice'
 
 function App() {
+  const dispatch = useDispatch()
+  const mode = useSelector((state) => state.mode)
   
+  function toggleMode(){
+    if(mode.darkMode) {
+      dispatch(toggleLight())
+    } else {
+      dispatch(toggleDark())
+    }
+  }
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: mode.color3, color: 'black' }} className="App">
       <Nav />
+      <button onClick ={toggleMode}>Toggle Mode</button>
       <ContentWrapper />
       <Footer />
     </div>
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import modeReducer from './features/modeSlice'
+
+export const store  = configureStore({
+    reducer: {
+        mode: modeReducer,
+        
+    }
+})
